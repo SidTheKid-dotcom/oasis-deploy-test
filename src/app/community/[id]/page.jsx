67f4d826b9c1ca4604966508cb9fac1f45191883
pma2012 <anapatprabhu@gmail.com>
2024-06-16 18:10:35 +0530

    Communities Feaeture added

---
+++ src/app/community/[id]/page.jsx
@@ -0,0 +1,181 @@
+"use client";
+import { HiUsers } from "react-icons/hi";
+import { Toaster, toast } from "sonner";
+import { useEffect, useState } from "react";
+import { MdEdit } from "react-icons/md";
+import Link from "next/link";
+import { useAuth } from "@/context/authContext";
+import CommunityPosts from "@/components/community/CommunityPosts";
+import axios from "axios";
+export default function Page({ params }) {
+  const [community, setCommunity] = useState(null);
+  const [isFollowing, setIsFollowing] = useState(false);
+  const { token } = useAuth();
+  const handleFollowToggle = async () => {
+    try {
+      if (!isFollowing) {
+        const response = await axios.post(
+          "https://oasis-api.xyz/api/community/subscribe",
+          {
+            community_id: params.id,
+          },
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        );
+
+        console.log(response);
+        setIsFollowing(true);
+        toast("Subscription added", {
+          position: "top-right",
+          className:
+            "bg-black text-white pixel-text border border-solid border-green-400",
+        });
+        setCommunity((prevCommunity) => ({
+          ...prevCommunity,
+          isSubscribed: true,
+        }));
+      } else {
+        const response = await axios.delete(
+          "https://oasis-api.xyz/api/community/unsubscribe",
+          {
+            headers: {
+              Authorization: token,
+            },
+            data: {
+              community_id: id,
+            },
+          }
+        );
+        console.log(response);
+        setIsFollowing(false);
+        setCommunity((prevCommunity) => ({
+          ...prevCommunity,
+          isSubscribed: false,
+        }));
+      }
+    } catch (error) {
+      console.error("Error during follow toggle:", error);
+    }
+  };
+
+  const getCommunity = async (id) => {
+    try {
+      const response = await axios.get(
+        `https://oasis-api.xyz/api/community?id=${id}`,
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+      const responseData = response.data;
+      console.log(responseData);
+      setCommunity(responseData);
+      setIsFollowing(responseData.isSubscribed);
+    } catch (error) {
+      console.error("Error fetching community data:", error);
+    }
+  };
+
+  useEffect(() => {
+    getCommunity(params.id);
+  }, [params.id, isFollowing]);
+
+  return (
+    <div className="md:mt-5 mt-4 md:w-[67%] md:mx-auto bg-black bg-opacity-80 md:bg-none md:bg-opacity-0 mx-3 mt-10 sm:mt-0  ">
+      <Toaster />
+      <div className="p-6">
+        {community && (
+          <div className="relative">
+            <div className="cursor-pointer">
+              <img
+                src={community.banner}
+                className="h-48 md:h-[270px] w-full"
+              />
+            </div>
+            <div className="flex justify-center cursor-pointer">
+              <img
+                src={community.icon}
+                alt=""
+                className="rounded-full h-20 w-20 md:h-28 md:w-28 z-20 absolute mt-[-9%] bg-black"
+              />
+            </div>
+            <div className="flex justify-between mt-14 text-lg ">
+              <p className="font-bold text-[#00B2FF] pixel-text">
+                {community.name}
+              </p>
+              <div className="flex my-auto ml-3">
+                <div className="my-auto">
+                  <HiUsers color="white" fill="white" fontSize="20px" />
+                </div>
+                <p className="text-white text-sm font-medium ml-1 pixel-text text-2xl">
+                  {community.no_of_subscribers}
+                </p>
+              </div>
+            </div>
+            <div className=" flex">
+              <img
+                src={community.creator.profile_picture}
+                alt=""
+                className="  mr-2 my-auto rounded-full h-10 w-10"
+              />
+              <p className=" text-white my-auto">
+                {community.creator.username}
+              </p>
+            </div>
+
+            <div className="flex justify-between mt-3 text-sm">
+              <div className="flex">
+                <div>
+                  <Link href="/create/post">
+                    {(community.amcreator || isFollowing) && (
+                      <button className="border-[1.5px] rounded-md p-1 mr-1 md:p-2 my-auto text-white  text-[0.5rem] border-[#767676] md:text-md pixel-text">
+                        Create Post +
+                      </button>
+                    )}
+                  </Link>
+                </div>
+
+                <p
+                  className={`text-white rounded-md my-auto md:p-2 border-[#767676] border-[1.5px] p-1 sm:font-semibold px-2 md:px-4 text-[0.5rem] md:text-bse text-center cursor-pointer pixel-text ${
+                    isFollowing ? "" : "bg-[#00b2ff]"
+                  }`}
+                  onClick={handleFollowToggle}
+                >
+                  {isFollowing ? "Subscribed" : "Subscribe"}
+                </p>
+
+                {community.amcreator && (
+                  <div className="my-auto">
+                    <Link href={`/EditCommunity/${community.id}`}>
+                      <div className="flex ml-2">
+                        <p className="my-auto text-white pixel-text text-xs md:text-md">
+                          Edit
+                        </p>
+                        <MdEdit
+                          color="#00b2ff"
+                          size={20}
+                          className="my-auto ml-1"
+                        />
+                      </div>
+                    </Link>
+                  </div>
+                )}
+              </div>
+              <p className="text-[#828282] text-sm pixel-text my-auto">
+                {community.type}
+              </p>
+            </div>
+          </div>
+        )}
+        <div className="text-white my-4 text-xs md:text-base">
+          {community && <p>{community.description}</p>}
+        </div>
+      </div>
+      {isFollowing && <CommunityPosts posts={community.posts} />}
+    </div>
+  );
+}
--- src/components/community/CommunityCard.jsx
+++ src/components/community/CommunityCard.jsx
@@ -1,95 +1,156 @@
-"use client"
+"use client";
+
+import axios from "axios";
+import Link from "next/link";
 import React, { useState } from "react";
 import { HiDotsVertical } from "react-icons/hi";
 import { HiUsers } from "react-icons/hi";
+import { useAuth } from "@/context/authContext";
+import { Toaster, toast } from "sonner";
 export default function ComunityCard({
-    key,
-    name,
-    description,
-    followers,
-    type,
-    icon,
+  key,
+  name,
+  description,
+  followers,
+  type,
+  icon,
+  id,
+  following,
 }) {
+  const [isfollowing, setIsFollowing] = useState(following);
+  const { token } = useAuth();
+  const handleFollowToggle = async () => {
+    try {
+      if (!isfollowing) {
+        const response = await axios.post(
+          "https://oasis-api.xyz/api/community/subscribe",
+          {
+            community_id: id,
+          },
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        );
+        toast("Subscription added", {
+          position: "top-right",
+          className:
+            "bg-black text-white pixel-text border border-solid border-green-400",
+        });
 
-    const [isfollowing, setIsFollowing] = useState(false);
+        setIsFollowing(true);
+      } else {
+        const response = await axios.delete(
+          "https://oasis-api.xyz/api/community/unsubscribe",
+          {
+            headers: {
+              Authorization: token,
+            },
+            data: {
+              community_id: id,
+            },
+          }
+        );
 
-    const handleFollowToggle = () => {
-        setIsFollowing(!isfollowing);
-      };
+        setIsFollowing(false);
+      }
+    } catch (error) {
+      console.error("Error during follow toggle:", error);
+    }
+  };
 
   return (
     <>
-    <div className="my-7" key={key}>
-      <div className="m-6 md:m-0 bg-black p-5  opacity-95 rounded-3xl  md:mr-16">
-        <div className="col-span-8 my-4">
-          <div className="mb-3">
-            <div className="flex justify-between">
-              <div className="flex w-full sm:justify-between">
-                <div className="rounded-full my-auto overflow-hidden flex">
-                  <img
-                    src={icon}
-                    alt=""
-                    className="  h-24  w-24 rounded-full "
-                  />
-                  <p className= " pixel-text text-[#00B2FF] font-bold text-sm md:text-lg   my-auto  ml-3 sm:ml-3  ">
-                  {name}
-                </p>
-                </div>
-                
-                <div className=" pixel-text hidden sm:block sm:flex">
-                  <p className="text-[#828282] text-sm md:text-md my-auto">
-                    {type}
-                  </p>
-                  <div className="flex my-auto ml-3">
-                    <div className="my-auto">
-                      <HiUsers
-                        color="white"
-                        fill="white"
-                        fontSize="18px"
-                      />
+      <Toaster />
+      <div className="my-4 " key={key}>
+        <div className=" md:m-0 bg-black p-3  opacity-95 rounded-3xl  md:mr-16">
+          <div className=" my-4">
+            <div className="mb-3">
+              <div className="">
+                {/* <div className="flex w-full sm:justify-between"> */}
+                <div className=" ">
+                  <div className="  md:grid grid-cols-12  gap-2 ">
+                    <div className=" my-auto overflow-hidden flex  col-span-5">
+                      <div>
+                        <Link href={`/community/${id}`}>
+                          {icon === "" ? (
+                            <img
+                              src="/profile2.png"
+                              alt=""
+                              className="  h-24  w-24 rounded-full "
+                            />
+                          ) : (
+                            <img
+                              src={icon}
+                              alt=""
+                              className="  h-24  w-24 rounded-full "
+                            />
+                          )}
+                        </Link>
+                      </div>
+                      <div className=" my-auto">
+                        <Link href={`/community/${id}`}>
+                          <p className=" pixel-text text-[#00B2FF] text-xs md:text-[1rem] font-bold text-md   my-auto  ml-5 sm:ml-3  ">
+                            {name}
+                          </p>
+                        </Link>
+                      </div>
                     </div>
-                    <p className="text-white text-sm font-medium ml-1">
-                      {followers}
+
+                    <p className="text-[#828282] text-sm md:text-md my-auto col-span-2 pixel-text hidden md:block">
+                      {type}
+                    </p>
+                    <div className="flex my-auto ml-3 col-span-3 pixel-text hidden md:block">
+                      <div className="my-auto flex">
+                        <HiUsers color="white" fill="white" fontSize="18px" />
+                        <p className="text-white text-sm font-medium ml-1">
+                          {followers}
+                        </p>
+                      </div>
+                    </div>
+
+                    <p
+                      className={`hidden md:block text-white rounded-md my-auto border-[#767676] border-[1.5px]  p-1 sm:font-semibold px-4  text-[10px]  sm:mr-4 ml-1  text-center col-span-2 cursor-pointer pixel-text ${
+                        isfollowing ? " px-0" : "bg-[#00B2FF]"
+                      }`}
+                      onClick={handleFollowToggle}
+                    >
+                      {isfollowing ? "Subscribed" : "Subscribe"}
                     </p>
                   </div>
                 </div>
+              </div>
 
+              <div className="flex mt-3 md:hidden justify-between">
+                <div className="flex my-auto">
+                  <p className="text-[#828282] pixel-text  text-xs my-auto">
+                    {type}
+                  </p>
+                  <div className="my-auto ml-4">
+                    <HiUsers color="white" fill="white" fontSize="15px" />
+                  </div>
+                  <p className="text-white text-xs font-medium ml-1 my-auto ">
+                    {followers}
+                  </p>
+                </div>
                 <p
-                  className={`hidden sm:block text-white rounded-md my-auto border-[#767676] border-[1.5px]  p-1 sm:font-semibold px-4  text-sm  sm:mr-4 ml-1 cursor-pointer pixel-text ${
-                    isfollowing ? 'bg-[#00B2FF]' : ''
+                  className={` ml-4 text-white rounded-md my-auto border-[#767676] border-[1.5px]  p-1 sm:font-semibold px-2 text-[7px] md:text-[10px]   cursor-pointer pixel-text ${
+                    isfollowing ? "" : "bg-[#00B2FF]"
                   }`}
                   onClick={handleFollowToggle}
                 >
-                  {isfollowing ? 'Following' : 'Follow'}
+                  {isfollowing ? "Subcribed" : "Subscribe"}
                 </p>
               </div>
             </div>
-
-            <div className="flex mt-3 sm:hidden">
-              <p className="text-[#828282] pixel-text  text-xs my-auto">
-                {type}
-              </p>
-              <div className="flex my-auto">
-                <div className="my-auto ml-4">
-                  <HiUsers color="white" fill="white" fontSize="15px" />
-                </div>
-                <p className="text-white text-xs font-medium ml-1 my-auto ">
-                  {followers}
-                </p>
-                <p className={`text-white my-auto border-[#767676] border-[1.5px] rounded-md p-[1.5px] text-xs px-4 ml-4 md:hidden pixel-text  ${
-                    isfollowing ? 'bg-[#00B2FF]' : ''
-                  }` }
-                  onClick={handleFollowToggle}>
-                  {isfollowing ? 'Following' : 'Follow'}
-                </p>
-              </div>
-            </div>
+            <p className="text-white   text-xs  ml-1  mt-3 md:text-base">
+              {description}
+            </p>
           </div>
-          <p className="text-white text-xs md:text-xs">{description}</p>
         </div>
       </div>
-    </div>
-    <hr className="border border-blue-500   mx-3 " />
-  </>
+      <hr className="border border-blue-500   mx-3 " />
+    </>
   );
 }
