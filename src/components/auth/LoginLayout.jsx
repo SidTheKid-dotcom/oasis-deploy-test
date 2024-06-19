32c878dd52c26051b01f7b1390e52ee9cf129f25
pma2012 <anapatprabhu@gmail.com>
2024-06-19 16:52:05 +0530

    auth page modified

--- src/components/auth/LoginLayout.jsx
+++ src/components/auth/LoginLayout.jsx
@@ -1,18 +1,22 @@
-'use client'
-import Form from './form';
+"use client";
+import Form from "./form";
 
 export default function LoginLayout() {
   return (
-    <div className="min-h-screen flex items-center justify-center pixel-text my-0">
+    <div className="min-h-screen flex items-center justify-center  my-0">
       <div className="glassmorphism md:p-8 flex flex-col md:flex-row w-full h-auto md:h-[700px]">
         <div className="flex flex-col justify-center items-center md:items-start  w-full md:w-1/2">
           <figure className="m-4 flex flex-row justify-center w-full mx-[-0.3rem]">
-            <img src='/logo.png' width="75px" height="75px" alt="Logo" />
+            <img src="/logo.png" width="75px" height="75px" alt="Logo" />
           </figure>
-          <div className='flex flex-row justify-center w-full'>
-            <div className="border border-white rounded-full text-white px-5 py-2">Oasis</div>
+          <div className="flex flex-row justify-center w-full">
+            <div className="border border-white rounded-full text-white px-5 py-2 pixel-text">
+              Oasis
+            </div>
           </div>
-          <div className="text-white mt-2 text-4xl text-center w-full">Welcome Aboard!</div>
+          <div className="text-white mt-2 text-4xl text-center w-full pixel-text">
+            Welcome Aboard!
+          </div>
         </div>
         <div className="flex flex-col justify-center items-center w-full md:w-1/2 mt-8 md:mt-0">
           <Form />
--- src/components/auth/form.jsx
+++ src/components/auth/form.jsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 import { useState } from "react";
 import { BeatLoader } from "react-spinners";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/context/authContext";
 
-const BASE_URL = 'https://oasis-api.xyz';
+const BASE_URL = "https://oasis-api.xyz";
 
 export default function Form() {
   const [mode, setMode] = useState("Sign In");
@@ -22,15 +22,15 @@ export default function Form() {
     try {
       setLoading(true);
       const response = await axios.post(BASE_URL + "/api/user/login", {
-        "user_cred": email,
-        "password": password
+        user_cred: email,
+        password: password,
       });
       const { data, status } = await response;
       setMessage(data.message);
       setLoading(false);
       if (status === 200 && data.token) {
-        login('Bearer ' + data.token);
-        router.push('/');
+        login("Bearer " + data.token);
+        router.push("/");
       }
     } catch (error) {
       setMessage(error.response.data.message);
@@ -43,12 +43,16 @@ export default function Form() {
       setLoading(true);
       if (password === conpassword) {
         const response = await axios.post(BASE_URL + "/api/user/register", {
-          "username": username,
-          "email": email,
-          "password": password
+          username: username,
+          email: email,
+          password: password,
         });
         const { data, status } = await response;
         setMessage(data.message);
+        if (status === 201 && data.token) {
+          login("Bearer " + data.token);
+          router.push("/");
+        }
         setLoading(false);
       } else {
         setMessage("Passwords do not match");
@@ -62,16 +66,26 @@ export default function Form() {
 
   return (
     <div className="bg-black p-8 w-full max-w-md mx-auto rounded-2xl text-white">
-      <div className="text-2xl text-center mb-6 text-white">{mode}</div>
+      <div className="text-2xl text-center mb-6 text-white pixel-text">
+        {mode}
+      </div>
       <div className="grid grid-cols-2 gap-4 mb-6">
         <button
-          className={`py-2 rounded ${mode === "Sign In" ? "bg-blue-500 text-white" : "bg-transparent border border-white text-white"}`}
+          className={`py-2 rounded pixel-text ${
+            mode === "Sign In"
+              ? "bg-blue-500 text-white"
+              : "bg-transparent border border-white text-white"
+          }`}
           onClick={() => setMode("Sign In")}
         >
           Sign In
         </button>
         <button
-          className={`py-2 rounded ${mode === "Sign Up" ? "bg-blue-500 text-white" : "bg-transparent border border-white text-white"}`}
+          className={`py-2 rounded pixel-text ${
+            mode === "Sign Up"
+              ? "bg-blue-500 text-white"
+              : "bg-transparent border border-white text-white"
+          }`}
           onClick={() => setMode("Sign Up")}
         >
           Sign Up
@@ -81,41 +95,105 @@ export default function Form() {
         <div>
           <div className="space-y-4">
             <div className="space-y-2">
-              <label htmlFor="email" className="block text-white">Email</label>
-              <input id="email" type="email" className="w-full px-4 py-2 rounded bg-gray-900 border border-gray-700 focus:outline-none focus:border-blue-500 text-white" onChange={(e) => setEmail(e.target.value)} />
+              <label htmlFor="email" className="block pixel-text text-white">
+                Email
+              </label>
+              <input
+                id="email"
+                type="email"
+                className="w-full px-4 py-2 rounded bg-gray-900 border border-gray-700 focus:outline-none focus:border-blue-500 text-white"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="space-y-2">
-              <label htmlFor="password" className="block text-white">Password</label>
-              <input id="password" type="password" className="w-full px-4 py-2 rounded bg-gray-900 border border-gray-700 focus:outline-none focus:border-blue-500 text-white" onChange={(e) => setPassword(e.target.value)} />
+              <label htmlFor="password" className="block text-white pixel-text">
+                Password
+              </label>
+              <input
+                id="password"
+                required
+                type="password"
+                className="w-full px-4 py-2 rounded bg-gray-900 border border-gray-700 focus:outline-none focus:border-blue-500 text-white"
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
           </div>
           <div className="mt-6 text-center">
-            <button className="w-full py-2 rounded bg-pink-500 hover:bg-pink-600 transition duration-300" onClick={signIn}>
+            <button
+              className="w-full py-2 rounded pixel-text bg-pink-500 hover:bg-pink-600 transition duration-300"
+              onClick={signIn}
+            >
               {loading ? <BeatLoader color="white" size={10} /> : "Sign In"}
             </button>
           </div>
+          <div className="mt-4 text-center">{message}</div>
         </div>
       )}
       {mode === "Sign Up" && (
         <div className="space-y-4 mt-6">
           <div className="space-y-2">
-            <label htmlFor="username" className="block text-white">Username</label>
-            <input id="username" type="text" className="w-full px-4 py-2 rounded bg-gray-900 border border-gray-700 focus:outline-none focus:border-blue-500 text-white" onChange={(e) => setUsername(e.target.value)} />
+            <label htmlFor="username" className="block text-white pixel-text">
+              Username
+            </label>
+            <input
+              id="username"
+              required
+              type="text"
+              className="w-full px-4 py-2 rounded bg-gray-900 border border-gray-700 focus:outline-none focus:border-blue-500 text-white"
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
           <div className="space-y-2">
-            <label htmlFor="signup-email" className="block text-white">Email</label>
-            <input id="signup-email" type="email" className="w-full px-4 py-2 rounded bg-gray-900 border border-gray-700 focus:outline-none focus:border-blue-500 text-white" onChange={(e) => setEmail(e.target.value)} />
+            <label
+              htmlFor="signup-email"
+              className="block text-white pixel-text"
+            >
+              Email
+            </label>
+            <input
+              id="signup-email"
+              required
+              type="email"
+              className="w-full px-4 py-2 rounded bg-gray-900 border border-gray-700 focus:outline-none focus:border-blue-500 text-white"
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="space-y-2">
-            <label htmlFor="signup-password" className="block text-white">Password</label>
-            <input id="signup-password" type="password" className="w-full px-4 py-2 rounded bg-gray-900 border border-gray-700 focus:outline-none focus:border-blue-500 text-white" onChange={(e) => setPassword(e.target.value)} />
+            <label
+              htmlFor="signup-password"
+              className="block pixel-text text-white"
+            >
+              Password
+            </label>
+            <input
+              id="signup-password"
+              required
+              type="password"
+              className="w-full px-4 py-2 rounded bg-gray-900 border border-gray-700 focus:outline-none focus:border-blue-500 text-white"
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div className="space-y-2">
-            <label htmlFor="confirm-password" className="block text-white">Confirm Password</label>
-            <input id="confirm-password" type="password" className="w-full px-4 py-2 rounded bg-gray-900 border border-gray-700 focus:outline-none focus:border-blue-500 text-white" onChange={(e) => setConpassword(e.target.value)} />
+            <label
+              htmlFor="confirm-password"
+              className="block pixel-text text-white"
+            >
+              Confirm Password
+            </label>
+            <input
+              id="confirm-password"
+              required
+              type="password"
+              className="w-full px-4 py-2 rounded bg-gray-900 border border-gray-700 focus:outline-none focus:border-blue-500 text-white"
+              onChange={(e) => setConpassword(e.target.value)}
+            />
           </div>
           <div className="mt-6 text-center">
-            <button className="w-full py-2 rounded bg-pink-500 hover:bg-pink-600 transition duration-300" onClick={signUp}>
+            <button
+              className="w-full py-2 pixel-text rounded bg-pink-500 hover:bg-pink-600 transition duration-300"
+              onClick={signUp}
+            >
               {loading ? <BeatLoader color="white" size={10} /> : "Sign Up"}
             </button>
           </div>
