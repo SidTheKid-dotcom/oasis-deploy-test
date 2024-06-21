a287c6b236f2e5d93593b1a04da6be97a7143390
pma2012 <anapatprabhu@gmail.com>
2024-06-21 10:32:39 +0530

    mobile nav issue fixed

--- src/components/layout/MobileNav.jsx
+++ src/components/layout/MobileNav.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { HiMenu } from "react-icons/hi";
 import { IoMdHome } from "react-icons/io";
 import { IoIosPeople } from "react-icons/io";
@@ -15,6 +15,8 @@ import Link from "next/link";
 import { useAuth } from "@/context/authContext";
 import { MdModeEdit } from "react-icons/md";
 import { IoIosCreate } from "react-icons/io";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 export default function MobileNav() {
   const pathname = usePathname();
   const { navBarData } = useAuth();
@@ -33,6 +35,57 @@ export default function MobileNav() {
     setRenderSearchBox(false);
   };
 
+  const [suggestions, setSuggestions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  const { token } = useAuth();
+
+  useEffect(() => {
+    const fetchSuggestions = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get(
+          "https://oasis-api.xyz/userRecommendations",
+          {
+            headers: {
+              Authorization: token,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        console.log(response.data);
+        setSuggestions(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching suggestions:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchSuggestions();
+  }, []);
+
+  const navigateUserProfile = (userId) => {
+    // Handle the logic for navigating to the user profile page
+    router.push("/profile/" + userId);
+  };
+
+  const handleFollowUser = async (userId) => {
+    await axios.post(
+      "https://oasis-api.xyz/api/user/follow",
+      { userId: userId },
+      {
+        headers: {
+          Authorization: token,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    setSuggestions((prevSuggestions) =>
+      prevSuggestions.filter((suggestion) => suggestion.id !== userId)
+    );
+  };
   return (
     <>
       <div className="sticky top-0 z-50 md:hidden ">
@@ -133,8 +186,67 @@ export default function MobileNav() {
                   </div>
                 </Link>
                 <hr className="border border-blue-500 mt-7" />
+                {/* <div>
+                 <PopularAccounts />
+                </div> */}
                 <div>
-                  <PopularAccounts />
+                  <div className="bg-black text-white rounded-[30px] p-5 pixel-text  my-10 z-10 mt-2 lg:mt-2 lg:sticky lg:top-2">
+                    <h2 className="text-2md font-bold mb-5">
+                      People You Might Know
+                    </h2>
+                    <div className=" ">
+                      {loading ? (
+                        <p>Loading...</p>
+                      ) : (
+                        <div className="flex flex-col gap-4 text-sm lg:overflow-y-auto scrollbar-hide lg:h-[400px] ">
+                          {suggestions.map((user) => (
+                            <div
+                              key={user.id}
+                              className="grid grid-cols-12 p-3 border border-slate-500 rounded-lg"
+                            >
+                              <div
+                                onClick={handleClick}
+                                className=" col-span-9"
+                              >
+                                {" "}
+                                <button
+                                  onClick={() => navigateUserProfile(user.id)}
+                                  className="  flex"
+                                >
+                                  <figure className="w-[40px] h-[40px] overflow-hidden rounded-full border border-white mr-2 my-auto">
+                                    {user.profile_picture === null ? (
+                                      <img
+                                        src={"/default_profile.jpg"}
+                                        alt={user.username}
+                                        className="w-full h-full object-cover"
+                                      />
+                                    ) : (
+                                      <img
+                                        src={user.profile_picture}
+                                        alt={user.username}
+                                        className="w-full h-full object-cover"
+                                      />
+                                    )}
+                                  </figure>
+
+                                  <div className="  text-xs my-auto break-words">
+                                    {user.username}
+                                  </div>
+                                </button>
+                              </div>
+
+                              <button
+                                onClick={() => handleFollowUser(user.id)}
+                                className=" text-sm col-span-3 border border-solid border-slate-100 rounded-md  hover:bg-blue-500 transition-colors"
+                              >
+                                Add
+                              </button>
+                            </div>
+                          ))}
+                        </div>
+                      )}
+                    </div>
+                  </div>
                 </div>
                 <div className="pixel-text">
                   <div className=" flex my-4">
